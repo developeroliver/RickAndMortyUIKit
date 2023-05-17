a08d82bdbcd02ef8d894c7c35929f540962976eb
olive <developpeurolive.ios@gmail.com>
2023-05-17 12:15:56 +0200

    Initial Commit

---
+++ RickAndMortyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyUIKit
+//
+//  Created by olivier geiger on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
