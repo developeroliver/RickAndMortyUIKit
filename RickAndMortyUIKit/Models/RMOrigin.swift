f5ce73b3f09b4d2e90be3a39b147a9cd3c29c9e5
olive <developpeurolive.ios@gmail.com>
2023-05-22 19:24:37 +0200

    Add Models

---
+++ RickAndMortyUIKit/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMortyUIKit
+//
+//  Created by olivier geiger on 22/05/2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMortyUIKit/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMortyUIKit
+//
+//  Created by olivier geiger on 22/05/2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
